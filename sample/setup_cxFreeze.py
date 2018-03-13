400c5eccffa5adbdfb2601974951bf1563522bf9
Daniel Schuette <d.schuette@online.de>
2018-03-12 22:28:18 -0400

    update setup.py files

--- sample/setup_cxFreeze.py
+++ sample/setup_cxFreeze.py
@@ -13,6 +13,7 @@ import packaging.specifiers
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 buildOptions = dict(includes = ["matplotlib.backends.backend_tkagg"],
+					include_files = ["../data/if_application-x-python_8974.icns", "../data/example_data.lsm"],
 					packages = ["Tkinter", "numpy.core._methods", "numpy.lib.format", "tkFileDialog", "matplotlib.style", "matplotlib.legend_handler", "FileDialog", "appdirs", "packaging"], 
 					excludes = [])
 
@@ -21,11 +22,11 @@ base = 'Win32GUI' if sys.platform=='win32' else None
 os.environ['TCL_LIBRARY'] = "/Library/Frameworks/Tcl.framework/Versions/8.6"
 
 executables = [
-    Executable('sample.py', base=base, targetName = 'CalciumImagingAnalyzer App')
+    Executable('sample.py', base=base, targetName = 'CalciumImagingAnalyzer App', icon="../data/if_application-x-python_8974.icns")
 ]
 
 setup(name='CalciumImagingAnalyzer',
       version = '0.02',
-      description = 'This is an application for analyzing calcium imaging data from cm confocal microscopy.',
+      description = 'This is an application for analyzing calcium imaging data generated via confocal microscopy.',
       options = dict(build_exe = buildOptions),
       executables = executables)
